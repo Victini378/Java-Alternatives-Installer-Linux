3e9928bbafdd6d56d264380a50f1883aa78882c1
Victini378 <victini378@gmail.com>
2019-11-08 19:54:56 +0100

    Added --help and --version commands

--- javaconfigure.py
+++ javaconfigure.py
@@ -1,5 +1,22 @@
 import subprocess, sys
 
+version = "javaconfigure 1.0"
+author = "Giuseppe Tomarchio aka victi, Victini378"
+title = f"{version}, {author}"
+
+if sys.argv[1] == "--help" or sys.argv[1] == "-h":
+        print(f"""\n{title}
+    
+        Usage: python3 javaconfigure.py [options] jdk_folder_name
+    
+        Options:
+        --version Show version information
+        --help, -h Show this message\n""")
+        exit()
+elif sys.argv[1] == "--version":
+    print (title)
+    exit()    
+
 bins = ["java", "javac", "jar", "jshell"]
 
 for bin in bins:
